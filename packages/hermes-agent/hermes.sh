4e14590bd121b1399353c658a6c186816115fe85
Chenxiong Qi <qcxhome@gmail.com>
2026-06-09 20:20:46 +0800

    fix(hermes-agent): Wrong skills and optional skills dir
    Signed-off-by: Chenxiong Qi <qcxhome@gmail.com>

--- packages/hermes-agent/hermes.sh
+++ packages/hermes-agent/hermes.sh
@@ -2,8 +2,8 @@
 
 declare -r DATA_DIR=/usr/share/hermes-agent
 
-: "{HERMES_BUNDLED_SKILLS:-${DATA_DIR}}"
-: "{HERMES_OPTIONAL_SKILLS:-${DATA_DIR}}"
+: "${HERMES_BUNDLED_SKILLS:=${DATA_DIR}/skills}"
+: "${HERMES_OPTIONAL_SKILLS:=${DATA_DIR}/optional-skills}"
 
 export HERMES_BUNDLED_SKILLS
 export HERMES_OPTIONAL_SKILLS
